99eace4add5945771c230537d7360aa725d89f5a
ChrisP <petitcolaschristoper@gmail.com>
2021-08-03 12:50:18 +0200

    Resolution de bug qui empecher un admin de se logger, test passé au vert

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -35,8 +35,11 @@ export default class Login {
   handleSubmitAdmin = e => {
     const user = {
       type: "Admin",
-      email: e.target.querySelector(`input[data-testid="employee-email-input"]`).value,
-      password: e.target.querySelector(`input[data-testid="employee-password-input"]`).value,
+
+      //MODIFICATION DANS QUERYSELECTOR EMPLOYEE REMPLACE PAR ADMIN
+
+      email: e.target.querySelector(`input[data-testid="admin-email-input"]`).value,
+      password: e.target.querySelector(`input[data-testid="admin-password-input"]`).value,
       status: "connected"
     }
     this.localStorage.setItem("user", JSON.stringify(user))
