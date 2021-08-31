d6786457093a2f8f76f860c3feb13ad318abc4be
ChrisP <petitcolaschristoper@gmail.com>
2021-08-31 14:20:19 +0200

    Révision de l'écriture des tests pour mieux respecter les conventions

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -7,19 +7,20 @@ import { ROUTES } from "../constants/routes.js";
 import userEvent from "@testing-library/user-event";
 import Logout from "../containers/Logout.js";
 import Login from "../containers/Login.js";
-import firebase from "../__mocks__/firebase.js"
+import firebase from "../__mocks__/firebase.js";
 
-describe("Given I am connected as an employee", () => {
+describe("Given I am a user connected as Employee", () => {
   const onNavigate = (pathname) => (document.body.innerHTML = ROUTES({ pathname }));
+
+  const user = {
+    email: "topher@gmail.com",
+    password: "wxcvbn",
+    status: "connected",
+    type: "Employee",
+  };
+
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", () => {
-      const user = {
-        email: "topher@gmail.com",
-        password: "wxcvbn",
-        status: "connected",
-        type: "Employee",
-      };
-
       localStorage.setItem("user", JSON.stringify(user));
       document.body.innerHTML = LoginUI();
 
@@ -32,12 +33,11 @@ describe("Given I am connected as an employee", () => {
       const btnSubmitEmployee = screen.getByTestId("employee-login-button");
       btnSubmitEmployee.addEventListener("click", (e) => handleSubmitEmployee);
       userEvent.click(btnSubmitEmployee);
-      if(document.body.innerHTML = BillsUI({data : [], loading : true})){
-        expect(screen.getAllByText("Loading...")).toBeTruthy()
+      if ((document.body.innerHTML = BillsUI({ data: [], loading: true }))) {
+        expect(screen.getAllByText("Loading...")).toBeTruthy();
       }
       const checkClass = screen.getByTestId("icon-window").classList.contains("active-icon");
       expect(checkClass).toBeTruthy();
-
     });
 
     test("Then bills should be ordered from earliest to latest", () => {
@@ -48,94 +48,83 @@ describe("Given I am connected as an employee", () => {
       const datesSorted = [...dates].sort(antiChrono);
       expect(dates).toEqual(datesSorted);
     });
-  });
 
-  test("When I click on icon-eye, i should see the proof join on bill", () => {
-    const html = BillsUI({ data: bills });
-    document.body.innerHTML = html;
+    describe("When I click on icon-eye", () => {
+      test("Then I should see the proof join on bill", () => {
+        const html = BillsUI({ data: bills });
+        document.body.innerHTML = html;
 
-    $.fn.modal = jest.fn();
+        $.fn.modal = jest.fn();
 
-    const bill = new Bill({ document, onNavigate, undefined, localStorage });
-    const handleClickIconEye = jest.fn(bill.handleClickIconEye);
+        const bill = new Bill({ document, onNavigate, undefined, localStorage });
+        const handleClickIconEye = jest.fn(bill.handleClickIconEye);
 
-    const iconEye = document.querySelectorAll(`div[data-testid="icon-eye"]`);
-    iconEye.forEach((icon) => {
-      icon.addEventListener("click", () => handleClickIconEye(icon));
-      fireEvent.click(icon);
+        const iconEye = document.querySelectorAll(`div[data-testid="icon-eye"]`);
+        iconEye.forEach((icon) => {
+          icon.addEventListener("click", () => handleClickIconEye(icon));
+          fireEvent.click(icon);
+        });
+
+        expect(handleClickIconEye).toHaveBeenCalled();
+        expect(screen.getByText("Justificatif")).toBeTruthy();
+      });
+    });
+    describe("When I click on newBill button", () => {
+      test("Then I should be on NewBill Page", () => {
+        document.body.innerHTML = BillsUI({ data: [] });
+
+        const addBill = new Bill({ document, onNavigate, undefined, localStorage });
+        const handleClickNewBill = jest.fn(addBill.handleClickNewBill);
+
+        const btnAddBill = screen.getByTestId("btn-new-bill");
+        btnAddBill.addEventListener("click", handleClickNewBill);
+        userEvent.click(btnAddBill);
+        expect(handleClickNewBill).toHaveBeenCalled();
+        expect(screen.getByText("Envoyer une note de frais")).toBeTruthy();
+      });
     });
 
-    expect(handleClickIconEye).toHaveBeenCalled();
-    expect(screen.getByText("Justificatif")).toBeTruthy();
+    describe("When I click on disconnect button", () => {
+      test("Then I should be disconnected and I should be on Login Page", () => {
+        const html = BillsUI({ data: [] });
+        document.body.innerHTML = html;
+
+        const logout = new Logout({ document, onNavigate, localStorage });
+
+        const handleClick = jest.fn(logout.handleClick);
+
+        const buttonLogout = document.getElementById("layout-disconnect");
+        buttonLogout.addEventListener("click", handleClick);
+        userEvent.click(buttonLogout);
+        expect(handleClick).toHaveBeenCalled();
+        expect(screen.getByText("Administration")).toBeTruthy();
+
+        const form = screen.getByTestId("form-employee");
+        expect(form).toBeDefined();
+      });
+    });
   });
 
-  test("When I click on newBill button , I should be on NewBill Page", () => {
-    const user = {
-      type: "Employee",
-      email: "azerty@test.com",
-      password: "azerty",
-      status: "connected",
-    };
-
-    document.body.innerHTML = BillsUI({ data: [] });
-
-    const addBill = new Bill({ document, onNavigate, undefined, localStorage });
-    const handleClickNewBill = jest.fn(addBill.handleClickNewBill);
-
-    const btnAddBill = screen.getByTestId("btn-new-bill");
-    btnAddBill.addEventListener("click", handleClickNewBill);
-    userEvent.click(btnAddBill);
-    expect(handleClickNewBill).toHaveBeenCalled();
-    expect(screen.getByText("Envoyer une note de frais")).toBeTruthy();
-  });
-
-  test("when I click on disconnect buutton, I should be disconnected and I should be on Login Page", () => {
-    const html = BillsUI({ data: [] });
-    document.body.innerHTML = html;
-
-    const logout = new Logout({ document, onNavigate, localStorage });
-
-    const handleClick = jest.fn(logout.handleClick);
-
-    const buttonLogout = document.getElementById("layout-disconnect");
-    buttonLogout.addEventListener("click", handleClick);
-    userEvent.click(buttonLogout);
-    expect(handleClick).toHaveBeenCalled();
-    expect(screen.getByText("Administration")).toBeTruthy();
-
-    const form = screen.getByTestId("form-employee");
-    expect(form).toBeDefined();
-  });
-
-  
-  
-});
-
-describe("Given I am a user connected as Employee", () => {
   describe("When I navigate in Bills page", () => {
     test("fetches bills from mock API GET", async () => {
-       const getSpy = jest.spyOn(firebase, "get")
-       const bills = await firebase.get()
-       expect(getSpy).toHaveBeenCalledTimes(1)
-       expect(bills.data.length).toBe(4)
-    })
+      const getSpy = jest.spyOn(firebase, "get");
+      const bills = await firebase.get();
+      expect(getSpy).toHaveBeenCalledTimes(1);
+      expect(bills.data.length).toBe(4);
+    });
     test("fetches bills from an API and fails with 404 message error", async () => {
-      firebase.get.mockImplementationOnce(() =>
-        Promise.reject(new Error("Erreur 404"))
-      )
-      const html = BillsUI({ error: "Erreur 404" })
-      document.body.innerHTML = html
-      const message = await screen.getByText(/Erreur 404/)
-      expect(message).toBeTruthy()
-    })
+      firebase.get.mockImplementationOnce(() => Promise.reject(new Error("Erreur 404")));
+      const html = BillsUI({ error: "Erreur 404" });
+      document.body.innerHTML = html;
+      const message = await screen.getByText(/Erreur 404/);
+      expect(message).toBeTruthy();
+    });
     test("fetches messages from an API and fails with 500 message error", async () => {
-      firebase.get.mockImplementationOnce(() =>
-        Promise.reject(new Error("Erreur 500"))
-      )
-      const html = BillsUI({ error: "Erreur 500" })
-      document.body.innerHTML = html
-      const message = await screen.getByText(/Erreur 500/)
-      expect(message).toBeTruthy()
-    })
-  })
-})
+      firebase.get.mockImplementationOnce(() => Promise.reject(new Error("Erreur 500")));
+      const html = BillsUI({ error: "Erreur 500" });
+      document.body.innerHTML = html;
+      const message = await screen.getByText(/Erreur 500/);
+      expect(message).toBeTruthy();
+    });
+  });
+});
--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -1,174 +1,151 @@
 import { fireEvent, screen, createEvent } from "@testing-library/dom";
 import BillsUI from "../views/BillsUI.js";
-import Bill from "../containers/Bills"
+import Bill from "../containers/Bills";
 import NewBillUI from "../views/NewBillUI.js";
 import NewBill from "../containers/NewBill.js";
 import userEvent from "@testing-library/user-event";
 import { ROUTES } from "../constants/routes.js";
-import firestore from "../app/Firestore.js"
+import firestore from "../app/Firestore.js";
 
 import "@testing-library/jest-dom";
 
-describe("Given I am connected as an employee I am on NewBill Page ", () => {
-  test("it should highlight logo newBill", () => {
-
-    const user = {
-      type: "Employee",
-      email: "azerty@test.com",
-      password: "azerty",
-      status: "connected",
-    };
-
-    document.body.innerHTML = BillsUI({ data: [] });
-
-    const onNavigate = pathname => document.body.innerHTML = ROUTES({pathname})
-
-    const addBill = new Bill({ document, onNavigate, undefined, localStorage });
-    const handleClickNewBill = jest.fn(addBill.handleClickNewBill);
-    const btnAddBill = screen.getByTestId("btn-new-bill");
-    btnAddBill.addEventListener("click", handleClickNewBill);
-    userEvent.click(btnAddBill);
-    const emailIcon = screen.getByTestId("icon-mail");
-    const checkClass = emailIcon.classList.contains("active-icon");
-    expect(checkClass).toBeTruthy();
-  });
-});
-
-test("When I do not fill fields and I click on button send, I should stay on NewBill page ", () => {
-  const html = NewBillUI();
-  document.body.innerHTML = html;
-
+describe("Given I am a user connected as Employee and I am on NewBill Page ", () => {
   const user = {
     type: "Employee",
     email: "azerty@test.com",
     password: "azerty",
     status: "connected",
   };
+  describe("When I navigate on NewBill page", () => {
+    test("then I should see newBill icon  highlighted", () => {
+      document.body.innerHTML = BillsUI({ data: [] });
 
-  localStorage.setItem("user", JSON.stringify(user))
+      const onNavigate = (pathname) => (document.body.innerHTML = ROUTES({ pathname }));
 
-  const onNavigate = (pathname) => (document.body.innerHTML = ROUTES({ pathname }));
+      const addBill = new Bill({ document, onNavigate, undefined, localStorage });
+      const handleClickNewBill = jest.fn(addBill.handleClickNewBill);
+      const btnAddBill = screen.getByTestId("btn-new-bill");
+      btnAddBill.addEventListener("click", handleClickNewBill);
+      userEvent.click(btnAddBill);
+      const emailIcon = screen.getByTestId("icon-mail");
+      const checkClass = emailIcon.classList.contains("active-icon");
+      expect(checkClass).toBeTruthy();
+    });
+  });
+  describe("When I do not fill fields and I click on button send", () => {
+    test("Then I should stay on NewBill page ", () => {
+      const html = NewBillUI();
+      document.body.innerHTML = html;
 
-  const dataNoValid = new NewBill({document, onNavigate, undefined, localStorage})
-  const handleSubmit = jest.fn(dataNoValid.handleSubmit)
-  const mock = jest.fn().mockReturnValue("");
+      localStorage.setItem("user", JSON.stringify(user));
 
-  const inputs = [...document.querySelectorAll("input")];
-  const fillInput = inputs.map((el) => (el.value = mock()));
+      const onNavigate = (pathname) => (document.body.innerHTML = ROUTES({ pathname }));
 
-  const buttonNewBill = document.getElementById("btn-send-bill");
+      const dataNoValid = new NewBill({ document, onNavigate, undefined, localStorage });
+      const handleSubmit = jest.fn(dataNoValid.handleSubmit);
+      const mock = jest.fn().mockReturnValue("");
 
-  
-  const result = fillInput.every((element) => element === "");
-  expect(result).toBeTruthy();
-  buttonNewBill.addEventListener("click", (e) => handleSubmit);
-  userEvent.click(buttonNewBill);
-  expect(screen.getByText("Mes notes de frais")).toBeTruthy()
+      const inputs = [...document.querySelectorAll("input")];
+      const fillInput = inputs.map((el) => (el.value = mock()));
 
-});
+      const buttonNewBill = document.getElementById("btn-send-bill");
 
-describe("I fill inputs with correct format except input file", () => {
-  test("I should stayed on NewBil Page and i'm invit to fill the inputs with wrong format", () => {
+      const result = fillInput.every((element) => element === "");
+      expect(result).toBeTruthy();
+      buttonNewBill.addEventListener("click", (e) => handleSubmit);
+      userEvent.click(buttonNewBill);
+      expect(screen.getByText("Mes notes de frais")).toBeTruthy();
+    });
+  });
 
-    const user = {
-      type: "Employee",
-      email: "azerty@test.com",
-      password: "azerty",
-      status: "connected",
-    };
+  describe("I fill inputs with correct format except input file", () => {
+    test("then I should stay on NewBil Page and i'm invit to fill the inputs with wrong format", () => {
+      localStorage.setItem("user", JSON.stringify(user));
 
-    localStorage.setItem("user", JSON.stringify(user))
+      const html = NewBillUI();
+      document.body.innerHTML = html;
 
-    const html = NewBillUI();
-    document.body.innerHTML = html;
+      const onNavigate = (pathname) => (document.body.innerHTML = ROUTES({ pathname }));
 
-    const onNavigate = pathname => document.body.innerHTML = ROUTES({pathname})
+      const nwBill = new NewBill({ document, onNavigate, undefined, localStorage });
 
-    const nwBill = new NewBill({document, onNavigate, undefined, localStorage})
+      const handleSubmit = jest.fn(nwBill.handleSubmit);
+      const buttonNewBill = document.getElementById("btn-send-bill");
 
-    const handleSubmit = jest.fn(nwBill.handleSubmit)
-    const buttonNewBill = document.getElementById("btn-send-bill");
+      const mock = jest.fn().mockReturnValueOnce("10/10/2021").mockReturnValueOnce(10).mockReturnValueOnce(20).mockReturnValueOnce("");
 
+      const inputData = (screen.getByTestId("datepicker").value = mock());
+      const inputPrice = (screen.getByTestId("amount").value = mock());
+      const inputTva = (screen.getByTestId("pct").value = mock());
+      const inputProof = (screen.getByTestId("file").file = mock());
+      expect(inputData).not.toBeNull();
+      expect(inputPrice).not.toBeNull();
+      expect(inputTva).not.toBeNull();
+      expect(inputProof).not.toBeNull();
 
-    const mock = jest.fn().mockReturnValueOnce("10/10/2021").mockReturnValueOnce(10).mockReturnValueOnce(20).mockReturnValueOnce("");
+      buttonNewBill.addEventListener("click", (e) => handleSubmit);
+      userEvent.click(buttonNewBill);
+      expect(screen.getByText("Mes notes de frais")).toBeTruthy();
+    });
+  });
 
-    const inputData = (screen.getByTestId("datepicker").value = mock());
-    const inputPrice = (screen.getByTestId("amount").value = mock());
-    const inputTva = (screen.getByTestId("pct").value = mock());
-    const inputProof = (screen.getByTestId("file").file = mock());
-    expect(inputData).not.toBeNull();
-    expect(inputPrice).not.toBeNull();
-    expect(inputTva).not.toBeNull();
-    expect(inputProof).not.toBeNull();
+  describe("I fill a proof with bad extension", () => {
+    test("Then it should alert the user that the format of proof is not correct and to empty the fileds", () => {
+      const aaa = [new File(["(⌐□_□)"], "chucknorris.svg", { type: "image/svg" })];
 
-    buttonNewBill.addEventListener("click",(e) => handleSubmit)
-    userEvent.click(buttonNewBill)
-    expect(screen.getByText("Mes notes de frais")).toBeTruthy()
+      const html = NewBillUI();
+      document.body.innerHTML = html;
+
+      const onNavigate = (pathname) => (document.body.innerHTML = ROUTES({ pathname }));
+
+      const inputFile = screen.getByTestId("file");
+
+      const addNewBill = new NewBill({ document, onNavigate, firestore, localStorage });
+      const handleChangeFile = jest.fn(addNewBill.handleChangeFile);
+
+      inputFile.addEventListener("input", handleChangeFile);
+      fireEvent(
+        inputFile,
+        createEvent("input", inputFile, {
+          target: { files: aaa },
+        })
+      );
+      expect(handleChangeFile).toHaveBeenCalled();
+      expect(screen.getByText("Veuillez saisir un format avec une extension valide(jpg, jpeg ou png)")).toBeDefined();
+    });
+  });
+
+  describe("I fill all fields with correct format and i click on button send", () => {
+    test("Then It should redirect user on Bills Page and I should see my new bill on list of bill", () => {
+      const onNavigate = (pathname) => (document.body.innerHTML = ROUTES({ pathname }));
+
+      const file = {
+        file1: "src/assets/images/facturefreemobile.jpg",
+      };
+
+      localStorage.setItem("user", JSON.stringify(user));
+
+      const html = NewBillUI();
+      document.body.innerHTML = html;
+
+      const validBill = new NewBill({ document, onNavigate, undefined, localStorage });
+      const handleSubmit = jest.fn(validBill.handleSubmit);
+
+      const mock = jest.fn().mockReturnValueOnce("10/10/2021").mockReturnValueOnce(10).mockReturnValueOnce(20).mockReturnValueOnce(file.file1);
+      const btn = document.getElementById("btn-send-bill");
+
+      const inputs = [];
+
+      const inputData = (screen.getByTestId("datepicker").value = mock());
+      const inputPrice = (screen.getByTestId("amount").value = mock());
+      const inputTva = (screen.getByTestId("pct").value = mock());
+      const inputProof = (screen.getByTestId("file").file = mock());
+      inputs.push(inputData, inputPrice, inputTva, inputProof);
+      expect(!inputs.every((el) => el === "")).not.toBeNull();
+      btn.addEventListener("click", (e) => handleSubmit);
+      userEvent.click(btn);
+      expect(screen.getByText("Mes notes de frais")).toBeTruthy();
+    });
   });
 });
 
-describe("I input a proof with bad extension", () => {
-  test("Then it should alert the user that the format of proof is not correct and to empty the fileds", () => {
-    const aaa = [new File(['(⌐□_□)'], 'chucknorris.svg', {type: 'image/svg'})];
-   
-    const html = NewBillUI();
-    document.body.innerHTML = html;
-
-    const onNavigate = (pathname) => (document.body.innerHTML = ROUTES({ pathname }));
-
-    const inputFile = screen.getByTestId("file");
-
-    const addNewBill = new NewBill({ document, onNavigate, firestore, localStorage });
-    const handleChangeFile = jest.fn(addNewBill.handleChangeFile);
-    
-    inputFile.addEventListener("input", handleChangeFile);
-    fireEvent(inputFile,createEvent('input', inputFile, {
-    target: {files: aaa},
-      }))
-    expect(handleChangeFile).toHaveBeenCalled();
-    expect(screen.getByText("Veuillez saisir un format avec une extension valide(jpg, jpeg ou png)")).toBeDefined();
-   
-  });
-});
-
-describe("I fill all fields with correct format and i click on button send", () => {
-  test("It should redirect user on Bills Page and I should see my new bill on list of bill", () => {
-    const onNavigate = (pathname) => (document.body.innerHTML = ROUTES({ pathname }));
-
-    const file = {
-      file1: "src/assets/images/facturefreemobile.jpg",
-    };
-
-    const user = {
-      type: "Employee",
-      email: "azerty@test.com",
-      password: "azerty",
-      status: "connected",
-    };
-
-    localStorage.setItem("user", JSON.stringify(user));
-
-    const html = NewBillUI();
-    document.body.innerHTML = html;
-
-    const validBill = new NewBill({ document, onNavigate, undefined, localStorage });
-    const handleSubmit = jest.fn(validBill.handleSubmit);
-
-    const mock = jest.fn().mockReturnValueOnce("10/10/2021").mockReturnValueOnce(10).mockReturnValueOnce(20).mockReturnValueOnce(file.file1);
-    const btn = document.getElementById("btn-send-bill");
-
-    const inputs = [];
-
-    const inputData = (screen.getByTestId("datepicker").value = mock());
-    const inputPrice = (screen.getByTestId("amount").value = mock());
-    const inputTva = (screen.getByTestId("pct").value = mock());
-    const inputProof = (screen.getByTestId("file").file = mock());
-    inputs.push(inputData, inputPrice, inputTva, inputProof);
-    expect(!inputs.every((el) => el === "")).not.toBeNull();
-    btn.addEventListener("click", (e) => handleSubmit);
-    userEvent.click(btn);
-    expect(screen.getByText("Mes notes de frais")).toBeTruthy();
-  });
-});
-
-// TEST SUR LE RETOUR EN ARRIERE A ECRIRE//
